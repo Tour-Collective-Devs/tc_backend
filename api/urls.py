043b06fcfc27aeece05bcb4d880959c503687e07
Riley Mathews <rileymathews80@gmail.com>
2018-08-11 13:15:06 -0500

    added employers urls to api

--- api/urls.py
+++ api/urls.py
@@ -2,11 +2,16 @@ from .views import Genre_View
 # from .views import Employer_View
 from api import views
 from rest_framework.routers import DefaultRouter
+from django.urls import include, path
 router = DefaultRouter()
+
+urlpatterns = [
+    path('employer', include('employers.urls'))
+]
 
 
 router.register('genre', Genre_View, base_name='genre')
 # router.register('employer', Employer_View, base_name='employer')
 router.register('role', views.Role_View, base_name='role')
 
-urlpatterns = router.urls
+urlpatterns += router.urls
