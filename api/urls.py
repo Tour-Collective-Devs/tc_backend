48ec2b7c70f45a612fb92d055de55570035aaceb
JacobAndrewSmith92 <jacobandrewsmith92@gmail.com>
2018-08-20 11:53:39 -0500

    Working on crew member auth. Refactoring NEEDED

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from .views import Genre_View
 from api import views
 from employers.views import Employer_View
+from crew_members.views import Crew_Member_View
 from rest_framework.routers import DefaultRouter
 from django.urls import include, path
 router = DefaultRouter()
@@ -11,6 +12,7 @@ urlpatterns = [
 ]
 
 router.register('employer', Employer_View, base_name='employer')
+router.register('crew', Crew_Member_View, base_name='crew')
 router.register('genre', Genre_View, base_name='genre')
 router.register('role', views.Role_View, base_name='role')
 router.register('event', views.Event_View, base_name='event')
