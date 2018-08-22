194745c04d7679538df14dc07c8c4ea71f1bafe7
Riley Mathews <rileymathews80@gmail.com>
2018-08-22 17:09:05 -0500

    added employer model

---
+++ api/models/employer.py
@@ -0,0 +1,13 @@
+from django.db import models
+from users.models import User
+
+""" 
+    module: employer model
+    author: riley mathews
+    purpose: to hold the data model for employer profile
+"""
+
+class Employer(models.Model):
+    """ model to hold employer profile information """
+    organization_name = models.CharField(max_length=100)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
