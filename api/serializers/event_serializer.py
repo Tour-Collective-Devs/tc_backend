f924b4769f00d9f2afa872a10c1ee1772c16226c
JacobAndrewSmith92 <jacobandrewsmith92@gmail.com>
2018-08-29 17:12:04 -0500

    Embedded user employer when creating an event to automagically post with employer name

--- api/serializers/event_serializer.py
+++ api/serializers/event_serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from api.models import Event
 from .crew_member_serializer import CrewMemberSerializer
+from .employer_serializer import EmployerSerializer
 
 """
     module: event serializer
@@ -13,7 +14,8 @@ class EventSerializer(serializers.HyperlinkedModelSerializer):
     Serializer for the event class
     """
     crew_member = CrewMemberSerializer(many=True, read_only=True)
-    
+    employer = EmployerSerializer(many=False, read_only=True)
+
     class Meta:
         fields = (
             'id',
